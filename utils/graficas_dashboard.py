0354f4993697fc43e32dc1cf3c13a423ec8f3337
gestion-analisis-epl <aescalante@grupoepl.com.mx>
2025-12-08 10:21:09 -0600

    Add filter for positive vacancies in dataframe

--- utils/graficas_dashboard.py
+++ utils/graficas_dashboard.py
@@ -296,7 +296,13 @@ def grafica_embudo_fase_proceso(df_vacantes_filtrado):
             
             if not df.empty:
                 # Filtrar registros que NO sean "CONTRATADO"
-                df = df.loc[(df["fase_proceso"] != "CONTRATADO") & (df["estatus_solicitud"] != "PENDIENTE") & df['fecha_autorizacion'].notna() & (df["estatus_solicitud"] != "CANCELADO")]
+                df = df.loc[
+                (df["fase_proceso"] != "CONTRATADO") & 
+                (df["estatus_solicitud"] != "PENDIENTE") & 
+                df['fecha_autorizacion'].notna() & 
+                (df["estatus_solicitud"] != "CANCELADO") &
+                (df['vacantes_solicitadas'] > 0)
+                ]
                 
                 # Contar por fase de proceso
                 conteo = df['fase_proceso'].value_counts().reset_index()
